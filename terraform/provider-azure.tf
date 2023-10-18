58841546da986f6e1576c62cad8c00ecb1628bd5
Kledson Basso <kledsonhugo@gmail.com>
2023-10-18 16:56:14 -0300

    Azure provider backend

--- terraform/provider-azure.tf
+++ terraform/provider-azure.tf
@@ -13,6 +13,13 @@ terraform {
   #   container_name       = "tfstate"
   #   key                  = "terraform.tfstate"
   # }
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-github-actions-state"
+    storage_account_name = "terraformgithubactions"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+  }
 
 }
 
