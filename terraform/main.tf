cdd16e3f7b824b91af215866b673e0b8d2f36d53
Kledson Basso <kledsonhugo@gmail.com>
2023-10-18 15:50:14 -0300

    Provider azurerm.azure

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,11 @@
 resource "azurerm_resource_group" "rg-staticsite" {
+  provider = azurerm.azure
   name     = "rg-staticsite"
   location = "eastus"
 }
  
 resource "azurerm_storage_account" "stracctstaticsite" {
+  provider                 = azurerm.azure
   name                     = var.stracctstaticsite
   resource_group_name      = azurerm_resource_group.rg-staticsite.name
   location                 = "eastus"
@@ -17,6 +19,7 @@ resource "azurerm_storage_account" "stracctstaticsite" {
 }
  
 resource "azurerm_storage_blob" "index" {
+  provider               = azurerm.azure
   name                   = "index.html"
   storage_account_name   = azurerm_storage_account.stracctstaticsite.name
   storage_container_name = "$web"
@@ -26,6 +29,7 @@ resource "azurerm_storage_blob" "index" {
 }
 
 resource "azurerm_storage_blob" "error" {
+  provider               = azurerm.azure
   name                   = "error.html"
   storage_account_name   = azurerm_storage_account.stracctstaticsite.name
   storage_container_name = "$web"
